ed421a6b13ab55c0b614696829a9684b87b6a020
AgafonovSiberia <agafonov@chita.ru>
2022-10-05 00:07:26 +0900

    refactor to Docker

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import uvicorn
 
 from fastapi.responses import JSONResponse
 from fastapi import FastAPI
@@ -10,14 +11,12 @@ from app.schemas import Number
 
 app = FastAPI()
 
-
 @app.post("/convert")
 def number_convert(number: Number):
     xml_body = create_xml_body(ubi_num=number.ubi_num)
     words_response = convert_number_to_words(data_xml=xml_body)
     response_json = convert_to_json(data_xml=words_response.content)
     return JSONResponse(content=response_json)
-
 
 @app.get("/test")
 def start_test():
@@ -28,5 +27,3 @@ def start_test():
         response_dict = convert_json_to_dict(response=response)
         test_json[ubi_num_random] = response_dict.get("result")
     return JSONResponse(content=test_json)
-
-
